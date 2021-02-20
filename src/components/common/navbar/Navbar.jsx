8658001df36f9ea3352ae9508ed5d724a44c8df5
subash-minu <subash.kharel.mil@gmail.com>
2021-02-20 17:54:13 -0600

    first push

---
+++ src/components/common/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar (){
+    return (
+        <section className = "navbar">
+            <a href="/" className="navbar-item">Home</a>
+            <a href="/about" className="navbar-item">About</a>
+            <a href="/discussionBoard" className="navbar-item">Discussion</a>
+            <a href="/learnings" className="navbar-item">Learnings</a>
+            <a href="/blog" className="navbar-item">Blog</a>
+            <a href="/contact" className="navbar-item">Contact</a>
+        </section>
+    );
+}
+
+export default Navbar;
